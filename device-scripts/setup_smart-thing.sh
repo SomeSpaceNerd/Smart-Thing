a075a9e198367b9351a2b34c4adb6aa8f4937c1f
William Mulreed <52981362+SomeSpaceNerd@users.noreply.github.com>
2024-07-25 15:57:55 -0400

    Update setup_smart-thing.sh

--- device-scripts/setup_smart-thing.sh
+++ device-scripts/setup_smart-thing.sh
@@ -1,1 +1,15 @@
-.
+#!/bin/sh
+
+# Prepare and launch smart-thing GUI
+
+# need to be root
+if [ "$(id -u)" != "0" ]; then
+    echo "Must be run as root"
+    exit 1
+fi
+
+echo "Stopping superbird app, it is ok if this step fails"
+/etc/init.d/S95supervisord stop
+mv /etc/init.d/S95supervisord /etc/init.d/K95supervisord
+echo "Activating chroot"
+./scripts/activate-chroot.sh
